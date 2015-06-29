7c419f686347480cf634da4a6fa26855cfb73be0
ByungDae Sohn <miconblog@gmail.com>
2015-06-30 01:38:34 +0900

    browserify 모듈을 gulp-browserify 모듈로 변경해서 재작성

--- gulp/browserify.js
+++ gulp/browserify.js
@@ -1,30 +1,12 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var config = {
-  settings: {
-    transform: ['reactify', 'babelify']
-  },
-  src: ['./flux/base.jsx'],
-  dest: './public/js',
-  outputName: 'bundle.js'
-};
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
-var bundler = watchify(browserify(config.src, watchify.args));
-config.settings.transform.forEach(function(t) {
-  bundler.transform(t);
+gulp.task('browserify', function() {
+  gulp.src(['./flux/base.jsx'], { read: false })
+    .pipe(browserify({
+      transform: ['reactify', 'babelify']
+    }))
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('./public/js'))
 });
-
-gulp.task('browserify', bundle);
-bundler.on('update', bundle);
-
-function bundle() {
-  return bundler.bundle()
-  // log errors if they happen
-  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-  .pipe(source(config.outputName))
-  .pipe(gulp.dest(config.dest))
-}
--- gulp/build.js
+++ gulp/build.js
@@ -1,7 +1,2 @@
 var gulp = require('gulp');
 gulp.task('build', ['styles', 'browserify']);
-gulp.task('build-ci', ['styles', 'browserify'], function(done){
-
-  process.exit(1);
-
-});
